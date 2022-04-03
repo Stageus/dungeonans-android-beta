5783d75202a29fe6dad1812ec0c229c3bd193835
yongkingg <dydwns8064@naver.com>
2022-04-03 23:36:04 +0900

    ADD : viewpager, bottomnavigation in MainActivity

---
+++ app/src/main/java/com/example/dungeonans/Activity/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.dungeonans.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.viewpager2.widget.ViewPager2
+import com.example.dungeonans.PageAdapter.ViewPagerAdapter
+import com.example.dungeonans.R
+import com.example.dungeonans.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 페이저에 어댑터 연결
+        binding.pager.adapter = ViewPagerAdapter(this)
+
+        // 슬라이드하여 페이지가 변경되면 바텀네비게이션의 탭도 그 페이지로 활성화
+        binding.pager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bottomNavigationView.menu.getItem(position).isChecked = true
+                }
+            }
+        )
+
+        // 리스너 연결
+        binding.bottomNavigationView.setOnItemSelectedListener(this)
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_Community -> {
+                // ViewPager의 현재 item에 첫 번째 화면을 대입
+                binding.pager.currentItem = 0
+                return true
+            }
+            R.id.nav_Ask -> {
+                // ViewPager의 현재 item에 두 번째 화면을 대입
+                binding.pager.currentItem = 1
+                return true
+            }
+            R.id.nav_Blog -> {
+                // ViewPager의 현재 item에 세 번째 화면을 대입
+                binding.pager.currentItem = 2
+                return true
+            }
+            R.id.nav_MyProfile -> {
+                binding.pager.currentItem = 3
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dungeonans/Fragment/MyProfileFragment.kt
@@ -0,0 +1,16 @@
+package com.example.dungeonans.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.dungeonans.R
+
+
+class MyProfileFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.myprofilepage_fragment,container,false)
+        return view
+    }
+}
