e8072ff0aa1611aa2f476abed3d7f40b35e77da2
inhajuyoung <wndud4576@naver.com>
2022-04-04 14:02:00 +0900

    Add Login Layout

--- app/src/main/java/com/example/dungeonans/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/dungeonans/Activity/LoginActivity.kt
@@ -3,4 +3,5 @@ package com.example.dungeonans.Activity
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
 }
--- app/src/main/java/com/example/dungeonans/Activity/MainActivity.kt
+++ app/src/main/java/com/example/dungeonans/Activity/MainActivity.kt
@@ -7,6 +7,6 @@ import com.example.dungeonans.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.loginpage_fragment)
     }
 }
