4a7fabccc11ae774bbce4ee701ea1c776aa67e4d
yongkingg <dydwns8064@naver.com>
2022-04-04 16:54:02 +0900

    ADD : Communitypage Ui

--- app/src/main/java/com/example/dungeonans/Activity/MainActivity.kt
+++ app/src/main/java/com/example/dungeonans/Activity/MainActivity.kt
@@ -5,16 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.HorizontalScrollView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.dungeonans.PageAdapter.ViewPagerAdapter
 import com.example.dungeonans.R
 import com.example.dungeonans.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationBarView
 import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startbinding()
@@ -27,24 +29,34 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var topbar = findViewById<ConstraintLayout>(R.id.mainActivityPostingLayout)
+        var scrollview = findViewById<HorizontalScrollView>(R.id.hashtagHorizontalScrollView)
         when(item.itemId){
             R.id.nav_Community -> {
                 // ViewPager의 현재 item에 첫 번째 화면을 대입
                 binding.pager.currentItem = 0
+                topbar.visibility = View.VISIBLE
+                scrollview.visibility = View.VISIBLE
                 return true
             }
             R.id.nav_Ask -> {
                 // ViewPager의 현재 item에 두 번째 화면을 대입
                 binding.pager.currentItem = 1
+                topbar.visibility = View.VISIBLE
+                scrollview.visibility = View.VISIBLE
                 return true
             }
             R.id.nav_Blog -> {
                 // ViewPager의 현재 item에 세 번째 화면을 대입
                 binding.pager.currentItem = 2
+                topbar.visibility = View.VISIBLE
+                scrollview.visibility = View.VISIBLE
                 return true
             }
             R.id.nav_MyProfile -> {
                 binding.pager.currentItem = 3
+                topbar.visibility = View.GONE
+                scrollview.visibility = View.GONE
                 return true
             }
             // 나머지 => 반환 안됨.
@@ -54,7 +66,6 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         }
     }
 
-    // 이벤트 동작 안됨
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_search -> {
@@ -68,7 +79,7 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
             else -> super.onOptionsItemSelected(item)
         }
     }
-    //
+
 
 
     fun startbinding() {
@@ -90,6 +101,8 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         )
         // 리스너 연결
         binding.bottomNavigationView.setOnItemSelectedListener(this)
+
+
     }
 
     fun connectToolbar() {
@@ -97,10 +110,8 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         setSupportActionBar(toolbar)
         var actionBar = supportActionBar
         actionBar!!.setDisplayShowCustomEnabled(true)
-        actionBar!!.setDisplayShowTitleEnabled(false)
-
         //좌측 버튼 활성화 => 로고로 바꿀것.
-        actionBar!!.setDisplayHomeAsUpEnabled(true)
-
+        actionBar!!.setDisplayShowTitleEnabled(false)
     }
+
 }
