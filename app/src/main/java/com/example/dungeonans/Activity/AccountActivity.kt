2c32c8600d6b60d5798d57db3fab823af4774e62
inhajuyoung <wndud4576@naver.com>
2022-04-06 18:31:12 +0900

    Add : add login fragment

---
+++ app/src/main/java/com/example/dungeonans/Activity/AccountActivity.kt
@@ -0,0 +1,17 @@
+package com.example.dungeonans.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dungeonans.R
+
+class AccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.accountpage_activity)
+
+    }
+}
+
+
