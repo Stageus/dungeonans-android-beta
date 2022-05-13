ca112e3b9e17ddbb3e1118baf0a45f570c30ceb3
devdejavu <wndud4576@naver.com>
2022-05-13 21:15:46 +0900

    add : userToken get 함수 추가

--- app/src/main/java/com/example/dungeonans/Utils/PrefManager.kt
+++ app/src/main/java/com/example/dungeonans/Utils/PrefManager.kt
@@ -9,6 +9,35 @@ import com.google.gson.Gson
 object PrefManager {
     private const val SHARED_SEARCH_HISTORY = "shared_search_history"
     private const val KEY_SEARCH_HISTORY = "key_search_history"
+    private const val SHARED_ACCOUNT = "shared_account"
+    private const val KEY_ACCOUNT = "key_account"
+
+    fun storeUserToken(userToken: String) {
+        // 쉐어드 가져오기
+        val shared = App.instance.getSharedPreferences(SHARED_ACCOUNT, Context.MODE_PRIVATE)
+
+        // 쉐어드 에디터 가져오기
+        val editor = shared.edit()
+        editor.putString(KEY_ACCOUNT, userToken)
+        editor.apply()
+    }
+
+    fun getUserToken(): String {
+        val shared = App.instance.getSharedPreferences(SHARED_ACCOUNT, Context.MODE_PRIVATE)
+
+        return shared.getString(KEY_ACCOUNT, "").toString()
+    }
+
+    fun deleteUserToken() {
+        // 쉐어드 가져오기
+        val shared = App.instance.getSharedPreferences(SHARED_ACCOUNT, Context.MODE_PRIVATE)
+
+        val editor = shared.edit()
+        editor.remove(KEY_ACCOUNT)
+        editor.apply()
+    }
+
+
 
     // 검색 목록 저장
     fun storeSearchHistoryList(searchHistoryList: MutableList<SearchData>) {
